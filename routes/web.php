3cf3847b21ebc18b028505649b6e92f3239efdab
Regynald Zambrano <regynald.zambrano@gmail.com>
2024-11-02 18:42:47 -0500

    welcome page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\VideoController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
@@ -12,4 +13,6 @@ Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
 
-require __DIR__.'/auth.php';
+Route::resource('videos', VideoController::class);
+
+require __DIR__ . '/auth.php';
