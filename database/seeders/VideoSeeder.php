17788b8f5a388cccff9173f045faf890a46ea751
Regynald Zambrano <regynald.zambrano@gmail.com>
2024-11-03 14:26:45 -0500

    videos names in the seeder

--- database/seeders/VideoSeeder.php
+++ database/seeders/VideoSeeder.php
@@ -11,22 +11,22 @@ class VideoSeeder extends Seeder
     {
         $videos = [
             [
-                'title' => 'Video 1',
+                'title' => 'How to get 1-2 ETHs Per Day Passive',
                 'description' => 'Description for video 1.',
                 'url' => 'https://www.youtube.com/embed/W-nyNQ6b4wo', // Embed link
             ],
             [
-                'title' => 'Video 2',
+                'title' => 'The Complete TALL Stack Tutorial',
                 'description' => 'Description for video 2.',
                 'url' => 'https://www.youtube.com/embed/Ul3sfSDEt9U',
             ],
             [
-                'title' => 'Video 3',
+                'title' => 'Ibiza Summer Mix 2024',
                 'description' => 'Description for video 3.',
                 'url' => 'https://www.youtube.com/embed/L-EF29ACmOs',
             ],
             [
-                'title' => 'Video 4',
+                'title' => 'Así es vivir en las diminutas',
                 'description' => 'Description for video 4.',
                 'url' => 'https://www.youtube.com/embed/TibhkHG335w',
             ],
